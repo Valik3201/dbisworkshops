fae4ceb02fc05662cff51af0d822c5809c59a3c4
Valik <valikchern@ukr.net>
2018-11-18 20:03:07 +0200

    Update populate.sql

--- km-52/Chernetskyi_Valentyn/workshop1/sql/populate.sql
+++ km-52/Chernetskyi_Valentyn/workshop1/sql/populate.sql
@@ -1,51 +1,48 @@
-INSERT INTO university
+INSERT INTO "university"
 VALUES ('name1');
-INSERT INTO university
+INSERT INTO "university"
 VALUES ('name4');
-INSERT INTO university
+INSERT INTO "university"
 VALUES ('name3');
-INSERT INTO university
+INSERT INTO "university"
 VALUES ('name2');
 
-INSERT INTO fuculty
-VALUES ('nam1', 1);
+INSERT INTO "faculty"
+VALUES (1, 'nam1');
 
-INSERT INTO fuculty
-VALUES ('name2', 2);
+INSERT INTO "faculty"
+VALUES (2, 'name2');
 
-INSERT INTO fuculty
-VALUES ('name3', 3);
+INSERT INTO "faculty"
+VALUES (3, 'name3');
 
-INSERT INTO fuculty
-VALUES ('name4', 4);
+INSERT INTO "faculty"
+VALUES (4, 'name4');
 
-INSERT INTO group
-VALUES ('name1', 1);
-INSERT INTO group
-VALUES ('name2', 2);
-INSERT INTO group
-VALUES ('name3', 3);
-INSERT INTO group
-VALUES ('name4', 4);
-
-INSERT INTO group
-VALUES ('name2', 2);
+INSERT INTO "group"
+VALUES (1, 'name1');
+INSERT INTO "group"
+VALUES (2, 'name2');
+INSERT INTO "group"
+VALUES (3, 'name3');
+INSERT INTO "group"
+VALUES (4, 'name4');
 
 
-INSERT INTO student
-VALUES ('first_name1', 'first_name1', 'first_name1@com', 1);
-INSERT INTO student
-VALUES ('first_name2', 'first_name2', 'first_name2@com', 2);
-INSERT INTO student
-VALUES ('first_name3', 'first_name3', 'first_name3@com', 3);
-INSERT INTO student
-VALUES ('first_name4', 'first_name4', 'first_name4@com', 4);
+INSERT INTO "students"
+VALUES (1, 'first_name1@com', 'first_name1', 'first_name1');
+INSERT INTO "students"
+VALUES (2, 'first_name2@com', 'first_name2', 'first_name2');
+INSERT INTO "students"
+VALUES (3, 'first_name3@com', 'first_name3', 'first_name3');
+INSERT INTO "students"
+VALUES (4, 'first_name4@com', 'first_name4', 'first_name4');
 
-INSERT INTO users
-VALUES ('first_name4@com', 'first_name4', False);
-INSERT INTO users
-VALUES ('first_name5@com', 'first_name', False);
-INSERT INTO users
-VALUES ('first_nam4@com', 'first_name', False);
-INSERT INTO users
-VALUES ('first_name41@com', 'first_name1', True);
+INSERT INTO "users"
+VALUES (1, 'first_name5@com', 'first_name5', False);
+INSERT INTO "users"
+VALUES (2, 'first_name6@com', 'first_name6', False);
+INSERT INTO "users"
+VALUES (3, 'first_name7@com', 'first_name7', False);
+INSERT INTO "users"
+VALUES (4, 'first_name8@com', 'first_name8', True);
