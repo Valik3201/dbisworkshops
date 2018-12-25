19fcb2e6e3432a146be0659b52838fb4bd383aa9
Valik <valikchern@ukr.net>
2018-12-25 22:42:46 +0200

    Add files via upload

--- km-52/Chernetskyi_Valentyn/project/code/create.sql
+++ km-52/Chernetskyi_Valentyn/project/code/create.sql
@@ -1,75 +1,43 @@
-CREATE TABLE "fuculty" (
-  "id" SERIAL CONSTRAINT "pk_fuculty" PRIMARY KEY,
-  "name" VARCHAR(255) NOT NULL
+CREATE TABLE UNIVERSITY (
+  UNIVERSITY_NAME VARCHAR2(30),
+  CONSTRAINT PK_UNIVERSE PRIMARY KEY (UNIVERSITY_NAME)
 );
 
-CREATE TABLE "group" (
-  "id" SERIAL CONSTRAINT "pk_group" PRIMARY KEY,
-  "name" VARCHAR(255) NOT NULL
+CREATE TABLE FACULTY (
+  FACULTY_NAME VARCHAR2(30),
+  CONSTRAINT PK_FACULTY PRIMARY KEY (FACULTY_NAME)
 );
 
-CREATE TABLE "fuculty_group_id" (
-  "fuculty" INTEGER NOT NULL,
-  "group" INTEGER NOT NULL,
-  CONSTRAINT "pk_fuculty_group_id" PRIMARY KEY ("fuculty", "group")
+CREATE TABLE ST_GROUP (
+  GROUP_NAME VARCHAR2(30),
+  GROUP_YEAR NUMBER(4),
+  CONSTRAINT PK_GROUP PRIMARY KEY (GROUP_NAME)
 );
 
-CREATE INDEX "idx_fuculty_group_id" ON "fuculty_group_id" ("group");
-
-ALTER TABLE "fuculty_group_id" ADD CONSTRAINT "fk_fuculty_group_id__fuculty" FOREIGN KEY ("fuculty") REFERENCES "fuculty" ("id");
-
-ALTER TABLE "fuculty_group_id" ADD CONSTRAINT "fk_fuculty_group_id__group" FOREIGN KEY ("group") REFERENCES "group" ("id");
-
-CREATE TABLE "university" (
-  "id" SERIAL CONSTRAINT "pk_university" PRIMARY KEY,
-  "name" VARCHAR(255) NOT NULL
+CREATE TABLE UNI_USER (
+  USER_LOGIN VARCHAR2(8) NOT NULL,
+  USER_PASSWORD VARCHAR2(30) NOT NULL,
+  USER_EMAIL VARCHAR2(30) NOT NULL,
+  STATUS VARCHAR2(30) NOT NULL,
+  CONSTRAINT PK_USER PRIMARY KEY (USER_LOGIN)
 );
 
-CREATE TABLE "fuculty_university_id" (
-  "university" INTEGER NOT NULL,
-  "fuculty" INTEGER NOT NULL,
-  CONSTRAINT "pk_fuculty_university_id" PRIMARY KEY ("university", "fuculty")
+CREATE TABLE STUDENT (
+  STUDENT_FIRST_NAME VARCHAR2(15) NOT NULL,
+  STUDENT_LAST_NAME VARCHAR2(15) NOT NULL,
+  UNIVERSITY_NAME VARCHAR2(30) NOT NULL,
+  FACULTY_NAME VARCHAR2(30) NOT NULL,
+  GROUP_NAME VARCHAR2(30) NOT NULL,
 );
 
-CREATE INDEX "idx_fuculty_university_id" ON "fuculty_university_id" ("fuculty");
+ALTER TABLE UNI_USER ADD CONSTRAINT FK_STATUS
+FOREIGN KEY (STATUS) REFERENCES UNI_STATUS (STATUS);
 
-ALTER TABLE "fuculty_university_id" ADD CONSTRAINT "fk_fuculty_university_id__fuculty" FOREIGN KEY ("fuculty") REFERENCES "fuculty" ("id");
+ALTER TABLE STUDENT ADD CONSTRAINT FK__ST_UNIVERSITY
+FOREIGN KEY (UNIVERSITY_NAME) REFERENCES UNIVERSITY (UNIVERSITY_NAME);
 
-ALTER TABLE "fuculty_university_id" ADD CONSTRAINT "fk_fuculty_university_id__university" FOREIGN KEY ("university") REFERENCES "university" ("id");
+ALTER TABLE STUDENT ADD CONSTRAINT FK__ST_FACULTY
+FOREIGN KEY (FACULTY_NAME) REFERENCES FACULTY (FACULTY_NAME);
 
-CREATE TABLE "user" (
-  "id" SERIAL CONSTRAINT "pk_user" PRIMARY KEY,
-  "email" VARCHAR(255) UNIQUE,
-  "password" VARCHAR(255) NOT NULL,
-  "registered_on" DATE,
-  "admin" BOOLEAN
-);
-
-CREATE TABLE "student" (
-  "id" SERIAL CONSTRAINT "pk_student" PRIMARY KEY,
-  "email" VARCHAR(255) UNIQUE,
-  "first_name" VARCHAR(255) NOT NULL,
-  "last_name" VARCHAR(255) NOT NULL,
-  "user_1" INTEGER NOT NULL,
-  "user_2" INTEGER NOT NULL
-);
-
-CREATE INDEX "idx_student__user_1" ON "student" ("user_1");
-
-CREATE INDEX "idx_student__user_2" ON "student" ("user_2");
-
-ALTER TABLE "student" ADD CONSTRAINT "fk_student__user_1" FOREIGN KEY ("user_1") REFERENCES "user" ("id");
-
-ALTER TABLE "student" ADD CONSTRAINT "fk_student__user_2" FOREIGN KEY ("user_2") REFERENCES "user" ("id");
-
-CREATE TABLE "group_student_id" (
-  "group" INTEGER NOT NULL,
-  "student" INTEGER NOT NULL,
-  CONSTRAINT "pk_group_student_id" PRIMARY KEY ("group", "student")
-);
-
-CREATE INDEX "idx_group_student_id" ON "group_student_id" ("student");
-
-ALTER TABLE "group_student_id" ADD CONSTRAINT "fk_group_student_id__group" FOREIGN KEY ("group") REFERENCES "group" ("id");
-
-ALTER TABLE "group_student_id" ADD CONSTRAINT "fk_group_student_id__student" FOREIGN KEY ("student") REFERENCES "student" ("id")
+ALTER TABLE STUDENT ADD CONSTRAINT FK__ST_GROUP
+FOREIGN KEY (GROUP_NAME) REFERENCES GROUP (GROUP_NAME);
